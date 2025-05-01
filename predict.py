5e7fa910b9157f66540b095b9893d02c8b7aff4e
Ceejay Santos <7078ceejay@gmail.com>
2025-05-01 09:12:40 +0800

    model training study

---
+++ predict.py
@@ -0,0 +1,42 @@
+import cv2
+from ultralytics import YOLO
+
+
+cap = cv2.VideoCapture(0)
+
+
+model = YOLO('best.pt') 
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        print("Failed to grab frame")
+        break
+
+   
+    results = model(frame)[0]
+
+    
+    for box in results.boxes:
+        
+        x1, y1, x2, y2 = map(int, box.xyxy[0])
+        conf = float(box.conf[0])
+        cls = int(box.cls[0])
+        label = f'{model.names[cls]} {conf:.2f}'
+
+        
+        cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+        
+        cv2.putText(frame, label, (x1, y1 - 10),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 2)
+
+    
+    cv2.imshow('YOLOv8 Detection', frame)
+
+    
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Release resources
+cap.release()
+cv2.destroyAllWindows()
